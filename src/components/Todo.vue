d627e782421ee9816e1115b04df200e7c1e756d7
Neill Robson <neill.robson@pendo.io>
2020-06-08 11:11:04 -0400

    Add edit todo form

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,10 +1,23 @@
 <template>
     <div class="ui centered card">
-        <div class="content">
+        <div class="content" v-show="!isEditing">
             <div class="header">{{ todo.title }}</div>
             <div class="meta">{{ todo.project }}</div>
             <div class="extra content">
-                <span class="right floated edit icon"><i class="edit icon"></i></span>
+                <span class="right floated edit icon" v-on:click="showForm">
+                    <i class="edit icon"></i>
+                </span>
+            </div>
+        </div>
+        <div class="content" v-show="isEditing">
+            <div class="ui form">
+                <div class="field"><label for="">Title</label><input type="text" v-model="todo.title"></div>
+                <div class="field"><label for="">Project</label><input type="text" v-model="todo.project"></div>
+                <div class="ui two button attached buttons">
+                    <button class="ui basic blue button" v-on:click="hideForm">
+                        Close
+                    </button>
+                </div>
             </div>
         </div>
         <div class="ui bottom attached basic button green" v-show="todo.done">
@@ -18,6 +31,22 @@
 
 <script>
 export default {
-    props: ['todo']
+    props: ['todo'],
+
+    data() {
+        return {
+            isEditing: false
+        };
+    },
+
+    methods: {
+        showForm() {
+            this.isEditing = true;
+        },
+
+        hideForm() {
+            this.isEditing = false;
+        }
+    }
 }
 </script>
