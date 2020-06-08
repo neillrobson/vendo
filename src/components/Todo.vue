b14c2af8f1e9862a1cc708c189fb4e40057583b8
Neill Robson <neill.robson@pendo.io>
2020-06-08 09:29:07 -0400

    Refactor todo into separate file

---
+++ src/components/Todo.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="ui centered card">
+        <div class="content">
+            <div class="header">{{ todo.title }}</div>
+            <div class="meta">{{ todo.project }}</div>
+            <div class="extra content">
+                <span class="right floated edit icon"><i class="edit icon"></i></span>
+            </div>
+        </div>
+        <div class="ui bottom attached basic button green" v-show="todo.done">
+            Completed
+        </div>
+        <div class="ui bottom attached basic button red" v-show="!todo.done">
+            Complete
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['todo']
+}
+</script>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,30 +2,17 @@
     <div>
         <p>Completed Tasks: {{todos.filter(todo => todo.done === true).length}}</p>
         <p>Current Tasks: {{todos.filter(todo => todo.done === false).length}}</p>
-        <div class="ui centered card" v-for="todo in todos" :key="todo.id">
-            <div class="content">
-                <div class="header">{{ todo.title }}</div>
-                <div class="meta">{{ todo.project }}</div>
-                <div class="extra content">
-                    <span class="right floated edit icon"><i class="edit icon"></i></span>
-                </div>
-            </div>
-            <div class="ui bottom attached basic button green" v-show="todo.done">
-                Completed
-            </div>
-            <div class="ui bottom attached basic button red" v-show="!todo.done">
-                Complete
-            </div>
-        </div>
+        <Todo v-for="todo in todos" :todo="todo" :key="todo.id" />
     </div>
 </template>
 
 <script>
+import Todo from "./Todo"
+
 export default {
-    props: ["todos"]
+    props: ["todos"],
+    components: {
+        Todo
+    }
 }
 </script>
-
-<style>
-
-</style>
