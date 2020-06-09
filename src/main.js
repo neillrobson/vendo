0d00d3bf1faf9722f37057cc6f7d7ac63f32cc3b
Neill Robson <neill.robson@pendo.io>
2020-06-09 14:17:03 -0400

    Add Vuex

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import store from './store'
 import App from './App.vue'
 
 import 'semantic-ui-css/semantic.min.css'
@@ -7,5 +8,6 @@ import './scripts/pendo'
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  store,
+  render: h => h(App)
 }).$mount('#app')
