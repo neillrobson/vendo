79c3aaef92aba1a64dbd954268fe5b985fea8095
Neill Robson <neill.robson@pendo.io>
2020-06-15 12:03:30 -0400

    Add Axios

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
+import Axios from 'axios'
 import store from './store'
 import router from './router'
 import App from './App.vue'
 
 import 'semantic-ui-css/semantic.min.css'
 import './scripts/pendo'
+
+Vue.prototype.$http = Axios
 
 Vue.config.productionTip = false
 
