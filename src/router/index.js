d9f699689429f047a4f2d027e71c2be1c5a80d0f
Neill Robson <neill.robson@pendo.io>
2020-06-15 11:50:41 -0400

    Add Login template and URL redirect
    The login button itself is not yet functional.

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/Home.vue'
+import Login from '@/views/Login.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
   },
   {
     path: '/about',
@@ -16,7 +25,10 @@ Vue.use(VueRouter)
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    meta: {
+      requiresAuth: true
+    }
   }
 ]
 
@@ -24,6 +36,21 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
+
+function isLoggedIn() {
+  return localStorage.getItem('jwt') != null;
+}
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth) && !isLoggedIn()) {
+    next({
+      path: '/login',
+      params: { nextUrl: to.fullPath }
+    });
+  } else {
+    next();
+  }
+});
 
 export default router
---
+++ src/views/Login.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="ui middle aligned center aligned grid">
+        <div class="column">
+            <img src="@/assets/logo.png" alt="Vue logo">
+            <form class="ui large form">
+                <div class="ui stacked segment">
+                    <div class="field">
+                        <div class="ui left icon input">
+                            <i class="icon user"></i>
+                            <input type="text" name="username" placeholder="Username" v-model="username">
+                        </div>
+                    </div>
+                    <div class="field">
+                        <div class="ui left icon input">
+                            <i class="icon lock"></i>
+                            <input type="password" name="password" placeholder="Password" v-model="password">
+                        </div>
+                    </div>
+                    <button class="ui fluid large submit button" @click="handleSubmit">Log in</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .column {
+        max-width: 450px;
+    }
+</style>
+
+<script>
+export default {
+    data() {
+        return {
+            username: "",
+            password: ""
+        }
+    },
+    methods: {
+        handleSubmit(e) {
+            e.preventDefault();
+
+            if (this.password.length > 0) {
+                this.$http.post('/login', {
+                    username: this.username,
+                    password: this.password
+                }).then(response => {
+                    console.log(response);
+                    localStorage.setItem('user', JSON.stringify(response.data.user));
+                    localStorage.setItem('jwt', response.data.token);
+
+                    if (localStorage.getItem('jwt') != null && this.$route.params.nextUrl != null) {
+                        this.$router.push(this.$route.params.nextUrl);
+                    }
+                }).catch(error => {
+                    console.error(error.response);
+                })
+            }
+        }
+    }
+}
+</script>
