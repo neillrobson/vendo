337a747f9a27a959ac21276e3287b8d749793a6e
Neill Robson <neill.robson@pendo.io>
2020-06-17 15:46:53 -0400

    Add single login page test

---
+++ cypress/integration/login_page_spec.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+const USERNAME = 'neill'
+const BAD_PASSWORD = 'badpass'
+
+describe('The Login Page', () => {
+    it('displays an error when given invalid credentials', () => {
+        cy.visit('/login')
+        cy.get('input[name=username]').type(USERNAME)
+        cy.get('input[name=password]').type(BAD_PASSWORD)
+        cy.get('button[type=submit]').click()
+        cy.get('div.message.negative').contains('Invalid credentials')
+    })
+})
