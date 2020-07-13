374266159277d2130447bfdc05859cb325e5749c
Neill Robson <neill.robson@pendo.io>
2020-07-13 15:36:02 -0400

    Style login page

--- src/views/TodoApplication.vue
+++ src/views/TodoApplication.vue
@@ -1,17 +1,22 @@
 <template>
-  <div class="container mx-auto px-6 text-center">
-    <img class="mx-auto" alt="Vue logo" src="../assets/logo.png">
-    <TodoList />
-  </div>
+    <div class="container mx-auto px-6 text-center">
+        <img class="mx-auto" alt="Vue logo" src="../assets/logo.png">
+        <TodoList />
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import TodoList from '@/components/TodoList.vue'
 
 export default {
-  components: {
-    TodoList
-  }
+    components: {
+        TodoList
+    }
 }
 </script>
+
+<style lang="postcss" scoped>
+.container {
+    max-width: 450px;
+}
+</style>
