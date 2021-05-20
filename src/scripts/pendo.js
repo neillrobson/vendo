8bc013929b8efdb4695e893c0b11f381d4799343
Neill Robson <neill.robson@pendo.io>
2021-05-20 14:57:58 -0400

    Switch to pendo-dev API key
    We may want to switch this yet again to Apollo (perfserf) or another
    location, but for now I can't seem to create an app (or API key) in any
    more appropriate location. So I'll just re-use the onboarding app.

--- src/scripts/pendo.js
+++ src/scripts/pendo.js
@@ -1,14 +1,11 @@
 /* eslint-disable */
-(function (apiKey) {
-    (function (p, e, n, d, o) {
-        var v, w, x, y, z; o = p[d] = p[d] || {}; o._q = [];
-        v = ['initialize', 'identify', 'updateOptions', 'pageLoad']; for (w = 0, x = v.length; w < x; ++w)(function (m) {
-            o[m] = o[m] || function () { o._q[m === v[0] ? 'unshift' : 'push']([m].concat([].slice.call(arguments, 0))); };
-        })(v[w]);
-        y = e.createElement(n); y.async = !0; y.src = 'https://pendo-wildlings-static.storage.googleapis.com/agent/static/' + apiKey + '/pendo.js';
-        z = e.getElementsByTagName(n)[0]; z.parentNode.insertBefore(y, z);
-    })(window, document, 'script', 'pendo');
-})('697ae132-66e8-47f4-6858-96da721a7d3c');
+(function(apiKey){
+    (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=o._q||[];
+    v=['initialize','identify','updateOptions','pageLoad','track'];for(w=0,x=v.length;w<x;++w)(function(m){
+        o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
+        y=e.createElement(n);y.async=!0;y.src='https://pendo-dev-static.storage.googleapis.com/agent/static/'+apiKey+'/pendo.js';
+        z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
+})('bb43e99a-6582-4b01-47db-524988ff50c8');
 /* eslint-enable */
 
 export default function (userData) {
