d0cd03be40d61c42884560081d01595a447b5d9b
Neill Robson <neill.robson@pendo.io>
2021-08-04 15:17:45 -0400

    Dummy test updates

--- cypress/integration/intercept.spec.js
+++ cypress/integration/intercept.spec.js
@@ -1,19 +1,33 @@
 let count = 0;
 
 describe('Intercept logic', () => {
-    it('intercepts once, calls twice', () => {
+    beforeEach(() => {
         cy.intercept('**', req => {
-            req.reply(`<html><body><h1>${count}</h1></body></html>`);
+            req.reply(`<html><body style="display: flex; justify-content: center; align-items: center"><h1>${count}</h1></body></html>`);
         });
+    });
 
-        cy.visit('/zero').pause();
+    it('executes "then" statements in sequence', () => {
+        cy.visit('/not-zero').get('h1').should('contain', '1');
 
         ++count;
 
-        cy.visit('/one');
+        cy.visit('/one').get('h1').should('contain', '1');
 
         cy.then(() => ++count);
 
-        cy.visit('/two');
+        cy.visit('/two').get('h1').should('contain', '2');
     });
-})
+
+    it('remembers the state of previous tests', () => {
+        cy.visit('/two-again').get('h1').should('contain', '2');
+    });
+
+    it('parses the entire test before actually executing it', () => {
+        count = 5555555;
+
+        cy.visit('/jennifer').get('h1').should('contain', '8675309');
+
+        count = 8675309;
+    });
+});
