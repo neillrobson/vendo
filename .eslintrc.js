884becc24a3523e3dba6c5ae728cd57916479e63
Neill Robson <neill.robson@pendo.io>
2021-12-15 11:22:20 -0500

    Add Pretter back in, along with lint-staged
    The back-and-forth is killing me. However, this setup is designed to
    represent as many of the best practices as possible along with the
    general setup that Pendo itself uses.
    
    We may in the future update the pre-commit hooks to run autofixes too,
    but for now having the explicit console errors helps identify/debug
    VSCode's format-on-save capabilities and incongruencies.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,29 +3,14 @@ module.exports = {
     env: {
         node: true
     },
-    extends: [
-        'plugin:vue/vue3-recommended',
-        '@vue/standard'
-    ],
+    extends: ['plugin:vue/vue3-recommended', '@vue/standard', 'prettier'],
     parserOptions: {
         ecmaVersion: 2020,
         parser: '@babel/eslint-parser'
     },
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        indent: ['warn', 4],
-        'vue/html-indent': ['warn', 4],
-        'vue/html-closing-bracket-newline': ['warn', {
-            singleline: 'never',
-            multiline: 'never'
-        }],
-        semi: ['error', 'always'],
-        'space-before-function-paren': ['error', {
-            anonymous: 'always',
-            named: 'never',
-            asyncArrow: 'always'
-        }]
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
     },
     overrides: [
         {
