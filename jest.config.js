ffd239f1633ea730a12fa2880aecdb854227e0a6
Neill Robson <neill.robson@pendo.io>
2023-03-27 11:41:51 -0400

    Dependency updates galore

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,10 @@
 module.exports = {
+    testEnvironment: 'jsdom',
     transform: {
-        '^.+\\.vue$': 'vue-jest',
+        '^.+\\.vue$': '@vue/vue3-jest',
         '^.+\\js$': 'babel-jest'
     },
+    roots: ['<rootDir>/src/', '<rootDir>/tests/'],
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1'
     }
