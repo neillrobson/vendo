7a0ef8cebecf6e199f1a436fd93154ad6cfb7b54
Neill Robson <neill.robson@pendo.io>
2023-07-17 16:21:51 -0400

    Upgrade dependencies

--- src/views/Timezone.vue
+++ src/views/Timezone.vue
@@ -1,13 +1,15 @@
 <template>
     <div class="container grid grid-cols-2 gap-4">
         <div class="config">
-            <label for="active-day-reference">Active Day Reference (start)</label>
+            <label for="active-day-reference"
+                >Active Day Reference (start)</label
+            >
             <DatePicker v-model="activeDayReference">
                 <template #default="{ inputValue, inputEvents }">
                     <input
                         id="active-day-reference"
                         :value="inputValue"
-                        v-on="inputEvents">
+                        v-on="inputEvents" />
                 </template>
             </DatePicker>
             <label for="active-day-mask">Active Day Mask (base64)</label>
@@ -15,7 +17,7 @@
                 id="active-day-mask"
                 ref="activeDayMask"
                 v-model="activeDayMask"
-                type="text">
+                type="text" />
             <fieldset
                 class="border p-2"
                 :disabled="!activeDayCountEnabled || null">
@@ -24,10 +26,10 @@
                         id="active-day-count"
                         v-model="activeDayCountEnabled"
                         type="checkbox"
-                        class="inline w-auto m-0 mr-1">
-                    <label
-                        for="active-day-count"
-                        class="inline">Active Day Count</label>
+                        class="inline w-auto m-0 mr-1" />
+                    <label for="active-day-count" class="inline"
+                        >Active Day Count</label
+                    >
                 </legend>
                 <label for="active-day-count-first">First</label>
                 <DatePicker v-model="activeDayCountFirst">
@@ -35,25 +37,25 @@
                         <input
                             id="active-day-count-first"
                             :value="inputValue"
-                            v-on="inputEvents">
+                            v-on="inputEvents" />
                     </template>
                 </DatePicker>
                 <label for="active-day-count-period">Count</label>
                 <input
                     id="active-day-count-period"
                     v-model="activeDayCountCount"
-                    type="number">
+                    type="number" />
                 <label for="active-day-count-stride">Stride</label>
                 <input
                     id="active-day-count-stride"
                     v-model="activeDayCountStride"
                     type="number"
-                    min="1">
+                    min="1" />
                 <label for="active-day-count-window">Window</label>
                 <input
                     id="active-day-count-window"
                     v-model="activeDayCountWindow"
-                    type="number">
+                    type="number" />
             </fieldset>
         </div>
         <Calendar
@@ -65,6 +67,7 @@
 
 <script>
 import { Calendar, DatePicker } from 'v-calendar';
+import 'v-calendar/style.css';
 
 function daysBetween(to, from) {
     return Math.round((to - from) / 86400000);
@@ -98,7 +101,15 @@ function convertListToMask(list) {
 
     let str = '';
     for (let i = 0; i < rawMask.length; i += 8) {
-        str += String.fromCharCode(parseInt(rawMask.slice(i, i + 8).reverse().join(''), 2));
+        str += String.fromCharCode(
+            parseInt(
+                rawMask
+                    .slice(i, i + 8)
+                    .reverse()
+                    .join(''),
+                2
+            )
+        );
     }
 
     return btoa(str);
@@ -123,9 +134,17 @@ export default {
     computed: {
         rawMask() {
             try {
-                return atob(this.activeDayMask).split('').flatMap(
-                    c => c.charCodeAt(0).toString(2).padStart(8, '0').split('').reverse().map(d => parseInt(d))
-                );
+                return atob(this.activeDayMask)
+                    .split('')
+                    .flatMap((c) =>
+                        c
+                            .charCodeAt(0)
+                            .toString(2)
+                            .padStart(8, '0')
+                            .split('')
+                            .reverse()
+                            .map((d) => parseInt(d))
+                    );
             } catch (error) {
                 return [];
             }
@@ -145,9 +164,15 @@ export default {
                 return [];
             }
 
-            let startIndex = daysBetween(this.activeDayCountFirst, this.activeDayReference);
-            let endIndex = startIndex + this.activeDayCountCount * this.activeDayCountStride;
-            if (startIndex > endIndex) { // Accounting for negative counts (taking windows from before the first date)
+            let startIndex = daysBetween(
+                this.activeDayCountFirst,
+                this.activeDayReference
+            );
+            let endIndex =
+                startIndex +
+                this.activeDayCountCount * this.activeDayCountStride;
+            if (startIndex > endIndex) {
+                // Accounting for negative counts (taking windows from before the first date)
                 [startIndex, endIndex] = [endIndex, startIndex];
             }
 
@@ -159,15 +184,21 @@ export default {
                 maskIndex += this.activeDayCountStride
             ) {
                 let sliceEnd = Math.min(maskIndex + 1, this.rawMask.length);
-                let sliceStart = Math.max(maskIndex + 1 - this.activeDayCountWindow, 0);
-                if (sliceStart > sliceEnd) { // Accounting for negative window sizes (forward-looking windows)
+                let sliceStart = Math.max(
+                    maskIndex + 1 - this.activeDayCountWindow,
+                    0
+                );
+                if (sliceStart > sliceEnd) {
+                    // Accounting for negative window sizes (forward-looking windows)
                     [sliceStart, sliceEnd] = [sliceEnd, sliceStart];
                 }
                 if (sliceEnd <= 0 || sliceStart >= this.rawMask.length) {
                     continue;
                 }
 
-                const activeCount = this.rawMask.slice(sliceStart, sliceEnd).reduce((a, b) => a + b, 0);
+                const activeCount = this.rawMask
+                    .slice(sliceStart, sliceEnd)
+                    .reduce((a, b) => a + b, 0);
                 if (activeCount) {
                     const newDate = new Date(this.activeDayReference);
                     newDate.setDate(newDate.getDate() + maskIndex);
@@ -210,7 +241,9 @@ export default {
                 atob(value);
                 this.$refs.activeDayMask.setCustomValidity('');
             } catch (error) {
-                this.$refs.activeDayMask.setCustomValidity('Invalid Base64 string');
+                this.$refs.activeDayMask.setCustomValidity(
+                    'Invalid Base64 string'
+                );
             }
         }
     },
