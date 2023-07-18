2e0737181ca4b58d540918dd6021b409aef9dd2a
Neill Robson <neill.robson@pendo.io>
2023-07-18 08:26:30 -0400

    Fix Jest version mismatch

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     testEnvironment: 'jsdom',
+    testEnvironmentOptions: {
+        customExportConditions: ['node', 'node-addons']
+    },
     transform: {
         '^.+\\.vue$': '@vue/vue3-jest',
         '^.+\\js$': 'babel-jest'
